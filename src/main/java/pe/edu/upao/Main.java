c154d842786b20fc4428f4990ff3ef1122d1c11e
Ali Lozano <alilozanoc@gmail.com>
2023-04-15 09:40:46 -0500

    Main modificado con mi nombre

--- src/main/java/pe/edu/upao/Main.java
+++ src/main/java/pe/edu/upao/Main.java
@@ -2,6 +2,6 @@ package pe.edu.upao;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Ali!");
     }
 }
